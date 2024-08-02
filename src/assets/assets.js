3c1c4a48d39d59038d54104ed6e62ec5f89f6fbc
iamsansk <santhoshkumarb1309@gmail.com>
2024-08-02 07:21:51 +0530

    Assets js file edited

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -8,7 +8,6 @@ import menu_7 from './menu_7.png'
 import menu_8 from './menu_8.png'
 
 import food_1 from './food_1.jpg'
-import food_1 from './food_1.jpg'
 import food_2 from './food_2.jpg'
 import food_3 from './food_3.jpg'
 import food_4 from './food_4.jpg'
